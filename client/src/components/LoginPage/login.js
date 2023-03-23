ada1ed50093994c48bcb04aa65ce31066254cfed
leonelc13 <112787135+leonelc13@users.noreply.github.com>
2023-03-22 20:00:09 -0400

    updates to css

--- client/src/components/LoginPage/login.js
+++ client/src/components/LoginPage/login.js
@@ -16,23 +16,38 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!username && !password) {
+      setErrorMessage('Missing username and password');
+      return;
+    }
+
+    if (!username) {
+      setErrorMessage('Missing username');
+      return;
+    }
+  
+    if (!password) {
+      setErrorMessage('Missing password');
+      return;
+    }
+    /*
     try {
       /**const response = await fetch("http://localhost:3001/users?username=" + username + "&password=" + password);
       const data = await response.json(); */
-      if (data.length === 0 || users[0].password !== password || data[0].username !== username) {
+      /*if (data.length === 0 || users[0].password !== password || data[0].username !== username) {
         setErrorMessage("Sorry, we don't recognize that combination of username and password. Please try again");
         return;
       }
       // redirect to the homepage
     } catch (error) {
       console.error(error);
-    }
+    }*/
   };
 
   return (
     <div>
-      <h1>PennBuzz</h1>
-      {errorMessage && <p>{errorMessage}</p>}
+      <h1>Penn<a className="Buzz">Buzz</a></h1>
+      {errorMessage && <p className="error-text">{errorMessage}</p>}
 
       <form onSubmit={handleSubmit}>
         <p className="title-text">Sign In</p>
@@ -40,11 +55,11 @@ function Login() {
           Or <a href="/register">Sign up</a> to make your own account
         </p>
         <div>
-          <label>Username:</label>
+          <label className="titles-text">Username</label>
           <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
         <div>
-          <label>Password:</label>
+          <label className="titles-text">Password</label>
           <input type="password" value={password} onChange={handlePasswordChange} />
         </div>
         <button type="submit">Sign In</button>
--- client/src/components/RegisterPage/register.js
+++ client/src/components/RegisterPage/register.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import "./register.css";
 
 function Register() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -15,26 +16,63 @@ function Register() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform registration logic here
+
+    if (!username && !password) {
+      setErrorMessage('Please enter both a username and password');
+      return;
+    }
+
+    if (!username) {
+      setErrorMessage('Please enter a username');
+      return;
+    }
+  
+    if (!password) {
+      setErrorMessage('Please enter a password');
+      return;
+    }
+
+  
+   // implemented for faked backend
+   /* const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });*/
+  
+    //const data = await response.json();
+  
+   /* if (data.error) {
+      setErrorMessage(data.error);
+      return;
+    }*/
+  
   };
 
   return (
     <div>
-      <h1>Register</h1>
+      <h1>Penn<a className="Buzz">Buzz</a></h1>
+      {errorMessage && <p className='error-text'>{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
+        <p className="title-text">Sign Up To PennBuzz</p>
+        <p className="login-text">
+          Or <a href="/login">sign in</a> to your account
+        </p>
         <div>
-          <label>Username:</label>
+          <label className='titles-text'>Pick a Username</label>
           <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
         <div>
-          <label>Password:</label>
+          <label className='titles-text'>Pick a Password</label>
           <input type="password" value={password} onChange={handlePasswordChange} />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Sign Up</button>
       </form>
-      <p>
-        Already have an account? <Link to="/login">Login</Link>
-      </p>
     </div>
   );
 }
