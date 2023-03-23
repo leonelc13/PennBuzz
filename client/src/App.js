ed305a7b9aa21c470b33879975ced11a82011d00
Lorenzo Lucena Maguire <llucena@seas.upenn.edu>
2023-03-23 18:32:12 -0400

    Directory name correction

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import { Routes, Route, BrowserRouter as Router, Navigate} from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
 import MainFeed from './components/MainFeed/MainFeed';
 import DirectMessagingPage from './components/DirectMessaging/DirectMessagingPage';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import ProfilePage from './components/ProfilePage/ProfilePage'
 import Login from './components/LoginPage/login'
 import Register from './components/RegisterPage/register'
@@ -12,14 +12,14 @@ import './style/index.css';
 
 
 function App() {
-    
+
     const [authenticated, setAuthenticated] = useState(false);
     const [username, setUsername] = useState("");
 
     const handleLogout = () => {
         setAuthenticated(false);
     };
-    
+
     const handleLogin = (username) => {
         setUsername(username);
         setAuthenticated(true);
@@ -37,20 +37,20 @@ function App() {
         <Router>
             {authenticated ? (
                 <>
-                <Header {...props} />
-                <Routes>
-                    <Route exact path='/chat' element={<DirectMessagingPage {...props} />} />
-                    <Route exact path='/' element={<MainFeed {...props} />} />
-                    <Route exact path='/profile' element={<ProfilePage {...props} />} />
-                </Routes>
+                    <Header {...props} />
+                    <Routes>
+                        <Route exact path='/chat' element={<DirectMessagingPage {...props} />} />
+                        <Route exact path='/' element={<MainFeed {...props} />} />
+                        <Route exact path='/profile' element={<ProfilePage {...props} />} />
+                    </Routes>
                 </>
             ) : (
                 <>
-                <Routes>
-                    <Route exact path='/' element={<Navigate to='/login' />} />
-                    <Route exact path='/login' element={<Login handleLogin={handleLogin} />} />
-                    <Route exact path='/register' element={<Register handleLogin={handleLogin}/>} />
-                </Routes>
+                    <Routes>
+                        <Route exact path='/' element={<Navigate to='/login' />} />
+                        <Route exact path='/login' element={<Login handleLogin={handleLogin} />} />
+                        <Route exact path='/register' element={<Register handleLogin={handleLogin} />} />
+                    </Routes>
                 </>
             )}
         </Router>
