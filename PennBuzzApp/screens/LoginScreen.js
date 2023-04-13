b23418b1e8b97b848eae071252ce6f279260783f
leonelc13 <112787135+leonelc13@users.noreply.github.com>
2023-04-13 18:38:10 -0400

    Update LoginScreen.js

--- PennBuzzApp/screens/LoginScreen.js
+++ PennBuzzApp/screens/LoginScreen.js
@@ -52,7 +52,7 @@ function LoginScreen({ navigation, setUser }) {
         );
         return;
       }
-
+      setUser(username);
       navigation.navigate('ProfileScreen', { profile_id: username})
       setUser(username);
     } catch (error) {
